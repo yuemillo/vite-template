df7dc6b47b65c175d04ddba9252b0208ef011cf7
kulasisin <107702039@mail2.nccu.tw>
2024-02-06 16:26:03 +0800

    Updates

---
+++ assets/AboutView-gMRWViqP.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,e as o,w as a,f as e,F as r,b as c,r as _,o as u}from"./index-TqwRqy2I.js";const i={},l=c("h1",null,"This is About page.",-1);function p(d,f,m,b,h,x){const t=_("RouterLink");return u(),n(r,null,[l,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(i,[["render",p]]);export{V as default};
