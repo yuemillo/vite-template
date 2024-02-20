ff15ffc40387920ea759f8955de6385233db9cc0
emillo <yc.gd1688@gmail.com>
2024-02-20 19:25:53 +0800

    路由表修改無法正確顯示頁面

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,21 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue')
+    component: () => import('../views/FrontView.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/HomeView.vue'),
+      },
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('../views/AboutView.vue'),
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/AboutView.vue')
-  }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
